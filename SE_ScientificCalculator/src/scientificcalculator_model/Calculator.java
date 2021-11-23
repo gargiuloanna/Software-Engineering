2df92f2339bc72d3d04020ffd70361ed973e0c72
mdg217 <m.degregorio1702@gmail.com>
2021-11-23 23:52:36 +0100

    Update Calculator.java

--- SE_ScientificCalculator/src/scientificcalculator_model/Calculator.java
+++ SE_ScientificCalculator/src/scientificcalculator_model/Calculator.java
@@ -14,23 +14,51 @@ public class Calculator {
   
     public ComplexNumber stringToComplex(String s){
         
-        ComplexNumber c;
         double real = 0;
         double immaginary = 0;
         
         int sign_index = -1;
         
         //Prelevo la posizione di j se esiste nella stringa:
-        int j_index = s.indexOf('j');  
+        int j_index = s.indexOf('j');
+        System.out.println(j_index);
         
         //Prelevo il segno dall'espressione se esiste nella stringa:
         if (s.substring(1, s.length()).indexOf('+')!=-1)
             sign_index = s.substring(1, s.length()).indexOf('+')+1;
         else if (s.substring(1, s.length()).indexOf('-')!=-1)
-            sign_index = s.substring(1, s.length()).indexOf('-')+1;
+            sign_index = s.substring(1, s.length()).indexOf('-')+1;     
         
-        //Significa che il segno non è presente nell'operando, quindi abbiamo o parte R o parte Imm.
-        if (sign_index == -1){ 
+        if (sign_index != -1){
+            if (j_index<sign_index){ //Se j sta a sinistra significa che il primo numero è la parte immaginaria
+                if (s.substring(0, 1).equals("+")){
+                    immaginary = 1;
+                }
+                else if (s.substring(0, 1).equals("-")){
+                    immaginary = -1;
+                }
+                else if (s.substring(0, 1).equals("j")){
+                    immaginary = 1;
+                }
+                else{
+                    immaginary = Double.parseDouble(s.substring(0, s.indexOf('j')));
+                }
+                real = Double.parseDouble(s.substring(sign_index, s.length()));
+                System.out.println(real);
+            }
+            else if (j_index>sign_index){ //Se j sta a destra significa che il primo numero è la parte reale
+                if (s.substring(s.length()-1, s.length()).equals("j")){
+                    if (s.substring(s.length()-2, s.length()-1).equals("+") || s.substring(s.length()-2, s.length()-1).equals("-")){
+                        immaginary = Double.parseDouble(s.charAt(sign_index) + "1");
+                    }
+                    else{
+                        immaginary = Double.parseDouble(s.substring(sign_index, s.length()-1));
+                    }
+                real = Double.parseDouble(s.substring(0, sign_index));   
+                }
+            }
+        }
+        else if (sign_index == -1){ //Significa che il segno non è presente nell'operando, quindi abbiamo o parte R o parte Imm.
             if (j_index == -1){
                 real = Double.parseDouble(s);
                 immaginary = 0;
@@ -51,35 +79,6 @@ public class Calculator {
                 }
                 
             }
-            
-        }
-        
-        if (sign_index != -1){
-            if (j_index<sign_index){ //Se j sta a sinistra significa che il primo numero è la parte immaginaria
-                if (s.substring(0, 1).equals("+")){
-                    immaginary = 1;
-                }
-                else if (s.substring(0, 1).equals("-")){
-                    immaginary = -1;
-                }
-                else if (s.substring(0, 1).equals("j")){
-                    immaginary = 1;
-                }
-                else{
-                    immaginary = Double.parseDouble(s.substring(0, s.indexOf('j')));
-                }
-                real = Double.parseDouble(s.substring(sign_index, s.length()));
-            }
-            else if (j_index>sign_index){ //Se j sta a sinistra significa che il primo numero è la parte reale
-                if (s.substring(s.length()-1, s.length()).equals("j")){
-                    if (s.substring(s.length()-2, s.length()-1).equals("+") || s.substring(s.length()-2, s.length()-1).equals("-"))
-                        immaginary = Double.parseDouble(s.charAt(sign_index) + "1");
-                }
-                else{
-                    immaginary = Double.parseDouble(s.substring(sign_index, s.length()-1));
-                }
-                real = Double.parseDouble(s.substring(0, sign_index));    
-            }
         }
         
         return new ComplexNumber(real, immaginary);
@@ -87,5 +86,24 @@ public class Calculator {
     
     public ComplexNumber addition(ComplexNumber a, ComplexNumber b){
         return new ComplexNumber(a.getReal() + b.getReal(),a.getImaginary() + b.getImaginary());
+    }
+    
+    public ComplexNumber product(ComplexNumber a, ComplexNumber b){
+        double aa = a.getReal() * b.getReal();
+        double aib_1 = a.getReal() * b.getImaginary();
+        double aib_2 = a.getImaginary()* b.getReal();
+        double iibb = -1 * b.getImaginary() * a.getImaginary();
+        return new ComplexNumber((aa + iibb), (aib_1 + aib_2));
+    }
+    
+    public ComplexNumber conjugated(ComplexNumber x){
+        return new ComplexNumber(x.getReal(), -x.getImaginary());
+    }
+    
+    public ComplexNumber division(ComplexNumber a, ComplexNumber b){
+        ComplexNumber N = product(a, conjugated(b));
+        ComplexNumber D = product(b, conjugated(b));
+        
+        return null;
     }
 }
