626941ffa50ed421aceff9f9f20a55daf3681d08
ferra <ferra@172.19.215.72>
2021-11-23 16:06:59 +0100

    Commit Interfaccia

--- SE_ScientificCalculator/src/se_scientificcalculator/FXMLDocumentController.java
+++ SE_ScientificCalculator/src/se_scientificcalculator/FXMLDocumentController.java
@@ -11,6 +11,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -18,10 +20,12 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
+    private TextField addTextfiled;
+    @FXML
+    private ListView<?> cronologia;
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World hello!!");
@@ -33,5 +37,53 @@ public class FXMLDocumentController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void prodButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void subButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void addButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void invButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void sqrtButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void divButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void dupButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void dropButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void clearButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void overButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void swapButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void addOperand(ActionEvent event) {
+    }
     
 }
