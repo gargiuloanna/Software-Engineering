f41849a44e2ca20f51c0856f3c5ebdcc4a2f1447
mdg217 <m.degregorio1702@gmail.com>
2021-11-23 16:26:24 +0100

    Create CalculatorTest.java

---
+++ SE_ScientificCalculator/test/CalculatorTest.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import scientificcalculator_model.Calculator;
+import scientificcalculator_model.ComplexNumber;
+
+/**
+ *
+ * @author marco
+ */
+public class CalculatorTest {
+    
+    Calculator c;
+    
+    public CalculatorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        c = new Calculator();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    public void testStringToDouble(){
+        assertEquals(new ComplexNumber(12, 21), c.stringToComplex("12+21j"));
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
