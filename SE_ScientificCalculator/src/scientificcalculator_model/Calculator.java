5bf70454bd32bffa1742a23421f49b4b8c9a1439
mdg217 <m.degregorio1702@gmail.com>
2021-11-23 22:15:19 +0100

    Update Calculator.java

--- SE_ScientificCalculator/src/scientificcalculator_model/Calculator.java
+++ SE_ScientificCalculator/src/scientificcalculator_model/Calculator.java
@@ -11,53 +11,20 @@ package scientificcalculator_model;
 public class Calculator {
     
     
-    
-    
-    
-    
-    
-    
     public ComplexNumber stringToComplex(String s){
         
-        ComplexNumber c;
         double real = 0;
         double immaginary = 0;
         
         int sign_index = -1;
         
-        //Prelevo la posizione di j se esiste nella stringa:
-        int j_index = s.indexOf('j');  
-        
-        //Prelevo il segno dall'espressione se esiste nella stringa:
         if (s.substring(1, s.length()).indexOf('+')!=-1)
             sign_index = s.substring(1, s.length()).indexOf('+')+1;
+        
         else if (s.substring(1, s.length()).indexOf('-')!=-1)
             sign_index = s.substring(1, s.length()).indexOf('-')+1;
-        
-        //Significa che il segno non è presente nell'operando, quindi abbiamo o parte R o parte Imm.
-        if (sign_index == -1){ 
-            if (j_index == -1){
-                real = Double.parseDouble(s);
-                immaginary = 0;
-            }
-            else if (j_index != -1){
-                if (s.substring(0, 1).equals("+")){
-                    immaginary = 1;
-                }
-                else if (s.substring(0, 1).equals("-")){
-                    immaginary = -1;
-                }
-                else if (s.substring(0, 1).equals("j")){
-                    immaginary = 1;
-                }
-                else{
-                    immaginary = Double.parseDouble(s.substring(0, s.length()-1));
-                    real = 0;
-                }
-                
-            }
-            return new Complex();
-        }
+           
+        int j_index = s.indexOf('j');        
         
         if (sign_index != -1){
             if (j_index<sign_index){ //Se j sta a sinistra significa che il primo numero è la parte immaginaria
@@ -75,18 +42,42 @@ public class Calculator {
                 }
                 real = Double.parseDouble(s.substring(sign_index, s.length()));
             }
-            else if (j_index>sign_index){ //Se j sta a sinistra significa che il primo numero è la parte reale
+            else if (j_index>sign_index){ //Se j sta a destra significa che il primo numero è la parte reale
                 if (s.substring(s.length()-1, s.length()).equals("j")){
                     if (s.substring(s.length()-2, s.length()-1).equals("+") || s.substring(s.length()-2, s.length()-1).equals("-"))
                         immaginary = Double.parseDouble(s.charAt(sign_index) + "1");
                 }
                 else{
                     immaginary = Double.parseDouble(s.substring(sign_index, s.length()-1));
+                    System.out.println(immaginary);
                 }
-                real = Double.parseDouble(s.substring(0, sign_index));    
+                real = Double.parseDouble(s.substring(0, sign_index));  
+                System.out.println(real);
             }
         }
-        
+        else if (sign_index == -1){ //Significa che il segno non è presente nell'operando, quindi abbiamo o parte R o parte Imm.
+            if (j_index == -1){
+                real = Double.parseDouble(s);
+                immaginary = 0;
+                return new ComplexNumber(real, immaginary);
+            }
+            else if (j_index != -1){
+                if (s.substring(0, 1).equals("+")){
+                    immaginary = 1;
+                }
+                else if (s.substring(0, 1).equals("-")){
+                    immaginary = -1;
+                }
+                else if (s.substring(0, 1).equals("j")){
+                    immaginary = 1;
+                }
+                else{
+                    immaginary = Double.parseDouble(s.substring(0, s.length()-1));
+                    real = 0;
+                }
+                
+            }
+        }
         return new ComplexNumber(real, immaginary);
     }
     
