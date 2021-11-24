6e72522049ec4988dfa1f58fb8a954ea18f1b7fe
Luigina25 <>
2021-11-24 11:49:02 +0100

    Commit update methods testAddition() and implementation methods testSubtraction() and subtraction()

--- SE_ScientificCalculator/test/CalculatorTest.java
+++ SE_ScientificCalculator/test/CalculatorTest.java
@@ -64,8 +64,48 @@ public class CalculatorTest {
     
     @Test
     public void testAddition(){
+        //(a+bj) + (a+bj)
         assertEquals(new ComplexNumber(15, 4).getReal(), c.addition(new ComplexNumber(10, 2), new ComplexNumber(5, 2)).getReal(),0.0000001);
         assertEquals(new ComplexNumber(15, 4).getImaginary(), c.addition(new ComplexNumber(10, 2), new ComplexNumber(5, 2)).getImaginary(),0.0000001);
+        
+        //(a+bj) + a
+        assertEquals(new ComplexNumber(15, 4).getReal(), c.addition(new ComplexNumber(10, 4), new ComplexNumber(5, 0)).getReal(),0.0000001);
+        assertEquals(new ComplexNumber(15, 4).getImaginary(), c.addition(new ComplexNumber(10, 4), new ComplexNumber(5, 0)).getImaginary(),0.0000001);
+        
+        //(a+bj) + bj
+        assertEquals(new ComplexNumber(10, 4).getReal(), c.addition(new ComplexNumber(10, 2), new ComplexNumber(0, 2)).getReal(),0.0000001);
+        assertEquals(new ComplexNumber(10, 4).getImaginary(), c.addition(new ComplexNumber(10, 2), new ComplexNumber(0, 2)).getImaginary(),0.0000001);
+        
+        //a + a
+        assertEquals(new ComplexNumber(15, 0).getReal(), c.addition(new ComplexNumber(10, 0), new ComplexNumber(5, 0)).getReal(),0.0000001);
+        assertEquals(new ComplexNumber(15, 0).getImaginary(), c.addition(new ComplexNumber(10, 0), new ComplexNumber(5, 0)).getImaginary(),0.0000001);
+        
+        //bj + bj
+        assertEquals(new ComplexNumber(0, 4).getReal(), c.addition(new ComplexNumber(0, 2), new ComplexNumber(0, 2)).getReal(),0.0000001);
+        assertEquals(new ComplexNumber(0, 4).getImaginary(), c.addition(new ComplexNumber(0, 2), new ComplexNumber(0, 2)).getImaginary(),0.0000001);
+    }
+    
+    @Test
+    public void testSubtraction(){
+        //(a+bj) - (a+bj)
+        assertEquals(new ComplexNumber(5, 4).getReal(), c.subtraction(new ComplexNumber(10, 8), new ComplexNumber(5, 4)).getReal(),0.0000001);
+        assertEquals(new ComplexNumber(5, 4).getImaginary(), c.subtraction(new ComplexNumber(10,8), new ComplexNumber(5, 4)).getImaginary(),0.0000001);
+        
+        //(a+bj) - a
+        assertEquals(new ComplexNumber(5, 4).getReal(), c.subtraction(new ComplexNumber(10, 4), new ComplexNumber(5, 0)).getReal(),0.0000001);
+        assertEquals(new ComplexNumber(5, 4).getImaginary(), c.subtraction(new ComplexNumber(10, 4), new ComplexNumber(5, 0)).getImaginary(),0.0000001);
+        
+        //(a+bj) - bj
+        assertEquals(new ComplexNumber(10, 4).getReal(), c.subtraction(new ComplexNumber(10, 8), new ComplexNumber(0, 4)).getReal(),0.0000001);
+        assertEquals(new ComplexNumber(10, 4).getImaginary(), c.subtraction(new ComplexNumber(10, 8), new ComplexNumber(0, 4)).getImaginary(),0.0000001);
+        
+        //a - a
+        assertEquals(new ComplexNumber(10, 0).getReal(), c.subtraction(new ComplexNumber(15, 0), new ComplexNumber(5, 0)).getReal(),0.0000001);
+        assertEquals(new ComplexNumber(10, 0).getImaginary(), c.subtraction(new ComplexNumber(15, 0), new ComplexNumber(5, 0)).getImaginary(),0.0000001);
+        
+        //bj - bj
+        assertEquals(new ComplexNumber(0, 4).getReal(), c.subtraction(new ComplexNumber(0, 8), new ComplexNumber(0, 4)).getReal(),0.0000001);
+        assertEquals(new ComplexNumber(0, 4).getImaginary(), c.subtraction(new ComplexNumber(0, 8), new ComplexNumber(0, 4)).getImaginary(),0.0000001);
     }
     
     @Test
@@ -92,9 +132,5 @@ public class CalculatorTest {
         assertEquals(new ComplexNumber(-144, 0).getImaginary(), c.product(new ComplexNumber(0, 12), new ComplexNumber(0, 12)).getImaginary(), 0.0000001);
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+   
 }
