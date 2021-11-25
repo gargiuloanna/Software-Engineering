1fafc344b768dab2f878c7037ba24b04499de080
mdg217 <m.degregorio1702@gmail.com>
2021-11-25 10:10:39 +0100

    Create ArithmeticException.java

---
+++ SE_ScientificCalculator/src/calculator_exception/ArithmeticException.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Exception.java to edit this template
+ */
+package calculator_exception;
+
+/**
+ *
+ * @author marco
+ */
+public class ArithmeticException extends Exception{
+
+    /**
+     * Creates a new instance of <code>ArithmeticException</code> without detail
+     * message.
+     */
+    public ArithmeticException() {
+    }
+
+    /**
+     * Constructs an instance of <code>ArithmeticException</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public ArithmeticException(String msg) {
+        super(msg);
+    }
+}
