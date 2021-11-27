15699f083309ff603ebd7c6238eafba16447f6b2
mdg217 <m.degregorio1702@gmail.com>
2021-11-27 16:31:01 +0100

    Update ComplexNumberTest.java

--- SE_ScientificCalculator/test/ComplexNumberTest.java
+++ SE_ScientificCalculator/test/ComplexNumberTest.java
@@ -9,13 +9,14 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import scientificcalculator_model.ComplexNumber;
 
 /**
  *
  * @author marco
  */
 public class ComplexNumberTest {
-    
+
     public ComplexNumberTest() {
     }
     
@@ -34,10 +35,24 @@ public class ComplexNumberTest {
     @After
     public void tearDown() {
     }
+    
+    @Test
+    public void testStringToDouble(){
+        //a+bj
+        assertEquals(new ComplexNumber(15, 4).getReal(), ComplexNumber.stringToComplex("15+4j").getReal(), 0.0000001);
+        assertEquals(new ComplexNumber(15, 4).getImaginary(), ComplexNumber.stringToComplex("15+4j").getImaginary(), 0.0000001);
+        
+        //a
+        assertEquals(new ComplexNumber(15, 0).getReal(), ComplexNumber.stringToComplex("15").getReal(), 0.0000001);
+        assertEquals(new ComplexNumber(15, 0).getImaginary(), ComplexNumber.stringToComplex("15").getImaginary(), 0.0000001);
+        
+        //bj
+        assertEquals(new ComplexNumber(0, 4).getReal(), ComplexNumber.stringToComplex("4j").getReal(), 0.0000001);
+        assertEquals(new ComplexNumber(0, 4).getImaginary(), ComplexNumber.stringToComplex("4j").getImaginary(), 0.0000001);
+        
+        //bj+a
+        assertEquals(new ComplexNumber(15, 4).getReal(), ComplexNumber.stringToComplex("15+4j").getReal(), 0.0000001);
+        assertEquals(new ComplexNumber(15, 4).getImaginary(), ComplexNumber.stringToComplex("15+4j").getImaginary(), 0.0000001);
+    }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
 }
