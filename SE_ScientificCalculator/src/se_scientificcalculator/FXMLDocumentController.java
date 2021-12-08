0d92dea4b64a60f5db3a085db9278c90a545802c
mdg217 <m.degregorio1702@gmail.com>
2021-12-08 14:22:44 +0100

    Update FXMLDocumentController.java

--- SE_ScientificCalculator/src/se_scientificcalculator/FXMLDocumentController.java
+++ SE_ScientificCalculator/src/se_scientificcalculator/FXMLDocumentController.java
@@ -5,11 +5,11 @@
  */
 package se_scientificcalculator;
 
+import calculator_exception.CommandExistsException;
 import scientificcalculator_model.operationscommands.*;
 import java.io.*;
 import javafx.scene.input.KeyEvent;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -22,38 +22,34 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TextInputDialog;
-import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.FileChooser;
-
 import scientificcalculator_model.*;
-import scientificcalculator_model.statecalculator.CalculatorState;
-import scientificcalculator_model.statecalculator.OperationState;
-import scientificcalculator_model.statecalculator.State;
+import scientificcalculator_model.statecalculator.*;
 
 /**
  *
  * @author Anna
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     //GUI Elements
     @FXML
     private TextField addTextfield;
     @FXML
-    private ListView<ComplexNumber> history; 
+    private ListView<ComplexNumber> history;
     @FXML
     private ToggleGroup variables;
     @FXML
@@ -61,7 +57,7 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private GridPane radioGrid;
     @FXML
-    private Button addOperandButton;  
+    private Button addOperandButton;
     @FXML
     private Button exeOpButton;
     @FXML
@@ -76,38 +72,35 @@ public class FXMLDocumentController implements Initializable {
     private MenuItem cancelOp;
     @FXML
     private MenuItem SaveFile;
-    
+    @FXML
+    private Button addOperationTry;
+    @FXML
+    private Button stopInsert;
+
     //Memory Structures
     private ObservableList<Entry> list;
     private ComplexStack hist;
     private Map<Character, ComplexNumber> variableMemory;
     private Map<String, Operations> personalizedOperations;
     private String opName;
-    
+
     private Alert alertBox;
-    
+
     //Command Executer for the pattern implementation
     private ExecuteCommand exe;
-    
+
     //State for State Pattern implementation
     private State state;
-    
-    @FXML
-    private Button addOperationTry;
-    @FXML
-    private Button stopInsert;
-    
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-        
+
         variableMemory = new HashMap<>();
         personalizedOperations = new HashMap<>();
         list = FXCollections.observableArrayList();
         nameClm.setCellValueFactory(new PropertyValueFactory<>("name"));
         opClm.setCellValueFactory(new PropertyValueFactory<>("op"));
-        operationList.setItems(list); 
+        operationList.setItems(list);
         hist = new ComplexStack();
         history.setItems(hist.getMemory());
         alertBox = new Alert(Alert.AlertType.ERROR);
@@ -115,15 +108,17 @@ public class FXMLDocumentController implements Initializable {
         alertBox.setTitle("ERRORE");
         opName = "";
         exe = new ExecuteCommand();
+        addOperationTry.setDisable(false);
+        stopInsert.setDisable(true);
         
         setState(new CalculatorState());
-    }    
+    }
 
     @FXML
     private void prodButton(ActionEvent event) {
-       try {
-        state.product(hist, personalizedOperations, opName);
-        }catch(ArithmeticException e){
+        try {
+            state.product(hist, personalizedOperations, opName);
+        } catch (ArithmeticException e) {
             alertBox.setContentText("Inserire due operandi per effettuare l'operazione");
             alertBox.showAndWait();
         }
@@ -133,8 +128,8 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void subButton(ActionEvent event) {
         try {
-        state.subtraction(hist, personalizedOperations, opName);
-        }catch(ArithmeticException e){
+            state.subtraction(hist, personalizedOperations, opName);
+        } catch (ArithmeticException e) {
             alertBox.setContentText("Inserire due operandi per effettuare l'operazione");
             alertBox.showAndWait();
         }
@@ -143,8 +138,8 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void addButton(ActionEvent event) {
         try {
-        state.addition(hist, personalizedOperations, opName);
-        }catch(ArithmeticException e){
+            state.addition(hist, personalizedOperations, opName);
+        } catch (ArithmeticException e) {
             alertBox.setContentText("Inserire due operandi per effettuare l'operazione");
             alertBox.showAndWait();
         }
@@ -152,9 +147,9 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void invButton(ActionEvent event) {
-      try {
-        state.invert(hist, personalizedOperations, opName);
-        }catch(ArithmeticException e){
+        try {
+            state.invert(hist, personalizedOperations, opName);
+        } catch (ArithmeticException e) {
             alertBox.setContentText("Inserire un operando per effettuare l'operazione");
             alertBox.showAndWait();
         }
@@ -163,8 +158,8 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void sqrtButton(ActionEvent event) {
         try {
-        state.sqrt(hist, personalizedOperations, opName);
-        }catch(ArithmeticException e){
+            state.sqrt(hist, personalizedOperations, opName);
+        } catch (ArithmeticException e) {
             alertBox.setContentText("Inserire un operando per effettuare l'operazione");
             alertBox.showAndWait();
         }
@@ -174,8 +169,8 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void divButton(ActionEvent event) {
         try {
-        state.division(hist, personalizedOperations, opName);
-        }catch(ArithmeticException e){
+            state.division(hist, personalizedOperations, opName);
+        } catch (ArithmeticException e) {
             alertBox.setContentText("Inserire due operandi per effettuare l'operazione");
             alertBox.showAndWait();
         }
@@ -184,8 +179,8 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void dupButton(ActionEvent event) {
         try {
-        state.dup(hist, personalizedOperations, opName);
-        }catch(ArithmeticException e){
+            state.dup(hist, personalizedOperations, opName);
+        } catch (ArithmeticException e) {
             alertBox.setContentText("Inserire un operando per effettuare l'operazione");
             alertBox.showAndWait();
         }
@@ -193,9 +188,9 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void dropButton(ActionEvent event) {
-       try {
-        state.drop(hist, personalizedOperations, opName);
-        }catch(ArithmeticException e){
+        try {
+            state.drop(hist, personalizedOperations, opName);
+        } catch (ArithmeticException e) {
             alertBox.setContentText("Inserire un operando per effettuare l'operazione");
             alertBox.showAndWait();
         }
@@ -208,9 +203,9 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void overButton(ActionEvent event) {
-       try {
-        state.over(hist, personalizedOperations, opName);
-        }catch(ArithmeticException e){
+        try {
+            state.over(hist, personalizedOperations, opName);
+        } catch (ArithmeticException e) {
             alertBox.setContentText("Inserire due operandi per effettuare l'operazione");
             alertBox.showAndWait();
         }
@@ -218,19 +213,19 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void swapButton(ActionEvent event) {
-       try {
-        state.swap(hist, personalizedOperations, opName);
-        }catch(ArithmeticException e){
+        try {
+            state.swap(hist, personalizedOperations, opName);
+        } catch (ArithmeticException e) {
             alertBox.setContentText("Inserire due operandi per effettuare l'operazione");
             alertBox.showAndWait();
         }
     }
-    
-      @FXML
+
+    @FXML
     private void moduleButton(ActionEvent event) {
-       try {
-        state.module(hist, personalizedOperations, opName);
-        }catch(ArithmeticException e){
+        try {
+            state.module(hist, personalizedOperations, opName);
+        } catch (ArithmeticException e) {
             alertBox.setContentText("Inserire un operando per effettuare l'operazione");
             alertBox.showAndWait();
         }
@@ -239,36 +234,39 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void phaseButton(ActionEvent event) {
         try {
-        state.phase(hist, personalizedOperations, opName);
-        }catch(ArithmeticException e){
+            state.phase(hist, personalizedOperations, opName);
+        } catch (ArithmeticException e) {
             alertBox.setContentText("Inserire un operando per effettuare l'operazione");
             alertBox.showAndWait();
         }
     }
 
     @FXML
-    private void addOperand(ActionEvent event) {       
-        if(addTextfield.getText().isEmpty() || !(inputCheck(addTextfield.getText()))){
+    private void addOperand(ActionEvent event) {
+        if (addTextfield.getText().isEmpty() || !(inputCheck(addTextfield.getText()))) {
             alertBox.setContentText("Inserisci un numero valido");
             addTextfield.clear();
             alertBox.showAndWait();
-        }
-        else{            
+        } else {
             ComplexNumber operand = ComplexNumber.stringToComplex(addTextfield.getText());
-            state.push(hist,operand, personalizedOperations, opName);
+            state.push(hist, operand, personalizedOperations, opName);
             addTextfield.clear();
         }
     }
 
-    private char selectedVariable(){
+    private char selectedVariable() {
         String s = variables.getSelectedToggle().toString();
-        return s.charAt(s.length()-2);
+        return s.charAt(s.length() - 2);
     }
-    
+
     @FXML
     private void exeOperation(ActionEvent event) {
-        for(Command c: personalizedOperations.get(addOperation.getText()).getOpers()){
-            exe.execute(c);
+        try{
+            for (Command c : personalizedOperations.get(addOperation.getText()).getOpers())
+                exe.execute(c);
+        }catch(CommandExistsException | NullPointerException e){
+            alertBox.setContentText("L'operazione da eseguire non è valida o non è stata ancora inserita");
+            alertBox.showAndWait();
         }
     }
 
@@ -284,7 +282,7 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void addToLast(ActionEvent event) {
-       VariablesOperations.addToLast(hist, variableMemory, selectedVariable());
+        VariablesOperations.addToLast(hist, variableMemory, selectedVariable());
     }
 
     @FXML
@@ -294,17 +292,16 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void onEnter(KeyEvent event) {
-       if(event.getCode().equals(KeyCode.ENTER) || event.getCharacter().getBytes()[0] == '\n' || event.getCharacter().getBytes()[0] == '\r') {
-            if(addTextfield.getText().isEmpty()  || !(inputCheck(addTextfield.getText()))){
+        if (event.getCode().equals(KeyCode.ENTER) || event.getCharacter().getBytes()[0] == '\n' || event.getCharacter().getBytes()[0] == '\r') {
+            if (addTextfield.getText().isEmpty() || !(inputCheck(addTextfield.getText()))) {
                 alertBox.setContentText("Inserisci un numero valido");
                 addTextfield.clear();
                 alertBox.showAndWait();
-            }
-            else{            
+            } else {
                 ComplexNumber operand = ComplexNumber.stringToComplex(addTextfield.getText());
-                state.push(hist,operand, personalizedOperations, opName);
+                state.push(hist, operand, personalizedOperations, opName);
                 addTextfield.clear();
-            }                
+            }
         }
     }
 
@@ -314,25 +311,29 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void cancelOperation(ActionEvent event) {
+        Entry selectedEntry = operationList.getSelectionModel().getSelectedItem();
+        String selectedName = selectedEntry.getName();
+        personalizedOperations.remove(selectedName);
+        list.remove(selectedEntry);
     }
 
     @FXML
     private void saveOnFile(ActionEvent event) {
         FileChooser file = new FileChooser();
         File fileChosen = file.showSaveDialog(null);
-        PrintWriter writer; 
+        PrintWriter writer;
         if (fileChosen != null)
             try {
-                writer = new PrintWriter(fileChosen, "UTF-8");
-                for(String s : personalizedOperations.keySet()){
-                    writer.println(s + ": " + personalizedOperations.get(s).getOpers());
-                }
-                writer.close();
+            writer = new PrintWriter(fileChosen, "UTF-8");
+            for (String s : personalizedOperations.keySet()) {
+                writer.println(s + ": " + personalizedOperations.get(s).getOpers());
+            }
+            writer.close();
         } catch (FileNotFoundException ex) {
             Alert aler = new Alert(Alert.AlertType.ERROR);
             aler.setContentText("File not found");
             aler.setTitle("File Error");
-                    
+
         } catch (UnsupportedEncodingException ex) {
             Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -340,18 +341,41 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void addOperationTryMethod(ActionEvent event) {
+        addOperationTry.setDisable(true);
+        stopInsert.setDisable(false);
         setState(new OperationState());
         opName = addOperation.getText();
-        String str = "";
-        addOperation.clear();
-        if (opName.isEmpty()){
-            alertBox.setContentText("Inserire un nome valido");
-            alertBox.showAndWait();
+        if (personalizedOperations.containsKey(opName)){
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation Dialog");
+            alert.setHeaderText("Look, a Confirmation Dialog");
+            alert.setContentText("Are you ok with this?");
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK){
+                personalizedOperations.put(opName, new Operations());
+                for(Entry e: list){
+                    if(e.getName().equals(opName)){
+                        list.remove(e);
+                        break;
+                    }
+                }     
+            }
+            else{
+                System.out.println("L'operazione è annullata");
+                return;
+            }
         }
-        Operations o = new Operations();
-        personalizedOperations.put(opName, o);
+        System.out.println(opName);
+        if (opName.equals("")) {
+            alertBox.setContentText("L'operazione non esiste oppure non hai inserito il nome dell'operazione da creare");
+            alertBox.setTitle("Errore");
+            alertBox.setHeaderText("Manca il nome dell'operazione");
+            alertBox.showAndWait();
+            return;
+        }
         
-        
+        personalizedOperations.put(opName, new Operations());
         radioGrid.setDisable(true);
         exeOpButton.setDisable(true);
         history.setDisable(true);
@@ -359,33 +383,40 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void stopInsertMethod(ActionEvent event) {
+        addOperationTry.setDisable(false);
+        stopInsert.setDisable(true);
+        
         setState(new CalculatorState());
-        Entry e = new Entry(opName,personalizedOperations.get(opName));
-        list.add(e);
+        if (!personalizedOperations.isEmpty()) {
+            list.add(new Entry(opName, personalizedOperations.get(opName)));
+        }
         radioGrid.setDisable(false);
         exeOpButton.setDisable(false);
         history.setDisable(false);
     }
-    
+
     @FXML
     private void userDefinition(MouseEvent event) {
- 
         Entry e = operationList.getSelectionModel().getSelectedItem();
-        Command o = new UserOperationCommand(e.getName(),e.getOp());
+        Command o = new UserOperationCommand(e.getName(), e.getOp());
         personalizedOperations.get(opName).addOperation(o);
-               
+
     }
-       
-    private void setState(State s){
+
+    private void setState(State s) {
         state = s;
     }
-    private boolean inputCheck(String s){
-        for(int i=0; i<s.length(); i++)
-            if(s.charAt(i)>='A' && s.charAt(i)<='z')
-                if(! (s.charAt(i) == 'j'))
-                    return false;       
-        
+
+    private boolean inputCheck(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= 'A' && s.charAt(i) <= 'z') {
+                if (!(s.charAt(i) == 'j')) {
+                    return false;
+                }
+            }
+        }
+
         return true;
     }
-    
+
 }
