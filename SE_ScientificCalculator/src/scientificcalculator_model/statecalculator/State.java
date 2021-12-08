f703d47ba1fb9c8641b293123f5f98b82d0d95ac
mdg217 <m.degregorio1702@gmail.com>
2021-12-08 10:36:11 +0100

    Create State.java

---
+++ SE_ScientificCalculator/src/scientificcalculator_model/statecalculator/State.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package scientificcalculator_model.statecalculator;
+
+import java.util.Map;
+import scientificcalculator_model.ComplexStack;
+import scientificcalculator_model.operationscommands.Command;
+
+/**
+ *
+ * @author marco
+ */
+public abstract class State {
+    
+    public void addition(ComplexStack stack, Map opMemory, String opName){
+        
+    }
+    
+    public void subtraction(ComplexStack stack, Map opMemory, String opName){
+        
+    }
+    
+    public void product(ComplexStack stack, Map opMemory, String opName){
+        
+    }
+    
+    public void division(ComplexStack stack, Map opMemory, String opName){
+        
+    }
+    
+    public void invers(ComplexStack stack, Map opMemory, String opName){
+        
+    }
+}
