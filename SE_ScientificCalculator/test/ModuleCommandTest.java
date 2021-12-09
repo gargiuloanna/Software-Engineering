ee4cdea52f4828113889443a89abec3e7a25f7a2
mdg217 <m.degregorio1702@gmail.com>
2021-12-09 16:55:29 +0100

    Create ModuleCommandTest.java
    Refactor test for module operation converted in command

---
+++ SE_ScientificCalculator/test/ModuleCommandTest.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import scientificcalculator_model.ComplexNumber;
+import scientificcalculator_model.ComplexStack;
+import scientificcalculator_model.operationscommands.Command;
+import scientificcalculator_model.operationscommands.ExecuteCommand;
+import scientificcalculator_model.operationscommands.ModuleCommand;
+
+/**
+ *
+ * @author marco
+ */
+public class ModuleCommandTest {
+    
+    ExecuteCommand exe;
+    ComplexStack stack;
+       
+    ComplexNumber oper1;
+    ComplexNumber result;
+    
+    public ModuleCommandTest() {
+    }
+    
+    @Before
+    public void setUp() {
+        exe = new ExecuteCommand();
+        stack = new ComplexStack();
+        oper1 = new ComplexNumber(4, 4);
+    }
+
+    @Test 
+    public void testExecute(){
+        
+        //Test for (a+bj)
+        Command mod = new ModuleCommand(stack);
+        stack.push(oper1);
+        exe.execute(mod);
+        result = (ComplexNumber) stack.pop();
+        
+        assertEquals(new ComplexNumber(Math.sqrt(32), 0).getReal(), result.getReal(), 0.0000001);
+        assertEquals(new ComplexNumber(Math.sqrt(32), 0).getImaginary(), result.getImaginary(), 0.0000001);
+        stack.stackClear();
+    }
+}
