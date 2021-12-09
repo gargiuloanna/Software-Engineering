f4d581b16b7a7ec4af342b7b738bc311da54a6ff
mdg217 <m.degregorio1702@gmail.com>
2021-12-09 16:55:19 +0100

    Create ProductCommandTest.java
    Refactor test for product operation converted in command

---
+++ SE_ScientificCalculator/test/ProductCommandTest.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import scientificcalculator_model.ComplexNumber;
+import scientificcalculator_model.ComplexStack;
+import scientificcalculator_model.operationscommands.Command;
+import scientificcalculator_model.operationscommands.ExecuteCommand;
+import scientificcalculator_model.operationscommands.ProductCommand;
+
+/**
+ *
+ * @author marco
+ */
+public class ProductCommandTest {
+    
+    ExecuteCommand exe;
+    ComplexStack stack;
+       
+    ComplexNumber oper1;
+    ComplexNumber oper1_1;
+    ComplexNumber oper2;
+    ComplexNumber oper2_1;
+    ComplexNumber result;
+    
+    public ProductCommandTest() {
+    }
+
+    
+    @Before
+    public void setUp() {
+        exe = new ExecuteCommand();
+        stack = new ComplexStack();
+        oper1 = new ComplexNumber(4, 4);
+        oper1_1 = new ComplexNumber(2, 2);
+        
+        oper2 = new ComplexNumber(4, 4);
+        oper2_1 = new ComplexNumber(2, 0);
+    }
+
+    @Test
+    public void testExecute(){
+        Command pro = new ProductCommand(stack);
+        
+        //(a+bj)*(a+bj)
+        stack.push(oper1);
+        stack.push(oper1_1);
+        exe.execute(pro);
+        result = (ComplexNumber) stack.pop();
+        
+        assertEquals(new ComplexNumber(0, 16).getReal(), result.getReal() ,0.0000001);
+        assertEquals(new ComplexNumber(0, 16).getImaginary(), result.getImaginary() ,0.0000001);
+        
+        //(a+bj)*a
+        stack.push(oper2);
+        stack.push(oper2_1);
+        exe.execute(pro);
+        result = (ComplexNumber) stack.pop();
+        assertEquals(new ComplexNumber(8, 8).getReal(), result.getReal() ,0.0000001);
+        assertEquals(new ComplexNumber(8, 8).getImaginary(), result.getImaginary() ,0.0000001);
+        stack.stackClear();
+    }
+}
